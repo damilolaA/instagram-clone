0490f112e6b88f701e414c8d6ed1cc309b21629d
damilolo <adiodamilola@yahoo.com>
2018-06-05 17:56:10 -0700

    fix bug in check for identifying existing user

--- src/api/v1/user/user-model.js
+++ src/api/v1/user/user-model.js
@@ -3,7 +3,8 @@ exports.registerUser = (session, username, password) => {
 	return session.run('MATCH (user:User {username: {username}}) RETURN user', {username:username})
 		.then(result => {
 			console.log(result.records.length);
-			if(!result.records.length === 0) {
+			if(result.records.length) {
+				console.log('i got into the check');
 				throw {username: 'username already in use', status: 400}
 			} else {
 				return session.run('CREATE (user:User {username:{username}, password:{password}}) RETURN user', {
@@ -11,6 +12,7 @@ exports.registerUser = (session, username, password) => {
 					password: password
 				})
 				.then(result => {
+					console.log('i got into success');
 					return result.records[0].get('user');
 				})
 				.catch(error => {
